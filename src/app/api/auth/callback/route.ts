94963091d6013fbe7cd903be22f8ef781fe4b095
damattag <guilherme.damatta@yahoo.com.br>
2023-05-18 03:02:16 -0300

    profile view in home page

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,20 +2,24 @@ import { api } from "@/lib/api";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.nextUrl);
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
 
-  const registerResponse = await api.post("/register", { code });
+  const registerResponse = await api.post("/register", {
+    code,
+  });
 
   const { token } = registerResponse.data;
 
-  const redirectUrl = new URL("/", request.nextUrl);
+  const redirectURL = new URL("/", request.url);
 
-  const cookiesExpiresInSeconds = 60 * 60 * 24 * 30; // 1 month
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30; // 1 month
 
-  return NextResponse.redirect(redirectUrl, {
+  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", redirectURL);
+
+  return NextResponse.redirect(redirectURL, {
     headers: {
-      "Set-Cookie": `token=${token}; Path=/; max-age=${cookiesExpiresInSeconds};`,
+      "Set-Cookie": `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
     },
   });
 }
